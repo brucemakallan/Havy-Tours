5a85f8f235b20190ea3d04733a98ff20b8ee533e
Bruce Makaaru <brucemakallan@gmail.com>
2018-06-26 21:32:15 +0300

    Heroku Config 13

--- havytours/settings.py
+++ havytours/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # for /static/root/favicon.ico
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root')

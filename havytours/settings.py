e2d06b00a7da0f13c9495ce62c6cdb2a6fad4f0a
Bruce Makaaru <brucemakallan@gmail.com>
2018-06-26 21:35:54 +0300

    Heroku Config 15

--- havytours/settings.py
+++ havytours/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/app/static/'
+STATIC_URL = '/main/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
